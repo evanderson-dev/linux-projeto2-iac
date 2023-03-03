dad2a634638fc0be764d8d25412d68ab1ec5b1e1
evanderson-dev <axel_steel@live.com>
2023-03-03 21:04:50 +0000

    Apache v.1.0

---
+++ script-iac-02.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "ATUALIZANDO O SERVIDOR..."
+
+apt-get update
+apt-get upgrade -y
+apt-get install apache2
+apt-get unzip -y
+
+echo "BAIXANDO E COPIANDO OS ARQUIVOS DA APLICAÇÃO..."
+
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linux-site-dio-main
+cp -R * /var/www/html/
+cd /
+
+echo "FINALIZADO COM SUCESSO!"
